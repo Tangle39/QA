c2bfd0414c20ae5060ed6baa15636b97f1c49641
なな <lu@nana.local>
2019-02-15 19:13:10 +0800

    1

--- unitest/test_unit.py
+++ unitest/test_unit.py
@@ -4,15 +4,22 @@
 https://www.bilibili.com/video/av19430527/?p=9&t=1789
 '''
 from count import Count #后面一个是类，前一个是文件
+import true_f
 import unittest #单元测试框架
 
 class TestCount(unittest.TestCase):
     def setUp(self):
         self.j = Count(2,3)
-    def test_add(self):   #必须以test开头
+    def test_add(self):   #测试用例，必须以test开头
         self.add = self.j.add()
         self.assertEqual(self.add,5)
-        #assert断言  | https://www.cnblogs.com/feiyueNotes/p/7788995.html
+        #assert断言  | https://www.cnblogs.com/feiyueNotes/p/7788995.html，unittest提供了许多assert方法
+    #def test_add2(self):
+    #     self.add = self.j.add()
+    #     self.assertEqual(self.add,5.0)
+    def test_true(self):
+        self.prime =true_f.is_prime(2)
+        self.assertTrue(self.prime,msg='Is not prime~です')
 
     def tearDown(self):
         pass
@@ -21,3 +28,14 @@ class TestCount(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
+# 如何简单地理解Python中的if __name__ == '__main__'
+# https://blog.csdn.net/yjk13703623757/article/details/77918633
+
+    #运行所有的testcase
+
+'''   suite = unittest.TestSuite()
+    suite.addTest(TestCount('test_true'))
+#执行单个测试
+    runner=unittest.TextTestRunner()
+    runner.run(suite)
+'''
