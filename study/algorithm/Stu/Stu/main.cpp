df5649996b6bed430f65849fd3ea24604ab51b06
なな <lu@nana.local>
2019-03-17 15:25:26 +0800

    recall

---
+++ study/algorithm/Stu/Stu/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Stu
+//
+//  Created by なな on 2019/3/17.
+//  Copyright © 2019 なな. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
