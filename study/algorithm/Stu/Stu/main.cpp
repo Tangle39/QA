7d0cd08a1b83a97d2b5a46027484bae6ac73c5bd
なな <137413469@qq.com>
2019-03-21 14:23:42 +0800

    Update main.cpp

--- study/algorithm/Stu/Stu/main.cpp
+++ study/algorithm/Stu/Stu/main.cpp
@@ -7,9 +7,29 @@
 //
 
 #include <iostream>
+using namespace std;
 
+//使用泛型类型定义函数，称为函数模板
+template <class T>
+T sum(T a,T b)
+{
+    T result;
+    result = a+b;
+    return result;
+}
+void f1()
+{
+    int i =5,j=6,k;
+    double f=2.0,g=0.5,h;
+    k=sum(i, j);//k=sum<int>(i, j);
+    h=sum(f, g);
+    
+    cout<<k<< '\n';
+    cout<<h<< '\n';
+}
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    //std::cout << "Hello, World!\n";
+    f1();
     return 0;
 }
