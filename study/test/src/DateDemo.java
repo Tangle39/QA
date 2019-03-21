c0f85f1c608d92210d3d894a0dff4a07eccc73b5
なな <137413469@qq.com>
2019-03-21 18:48:25 +0800

    l

---
+++ study/test/src/DateDemo.java
@@ -0,0 +1,13 @@
+//使用 Date 对象的 toString() 方法来打印当前日期和时间
+import java.util.Date;
+
+public class DateDemo {
+    public static void main(String args[]) {
+        // 初始化 Date 对象
+        Date date = new Date();
+
+        // 使用 toString() 函数显示日期时间
+        System.out.println(date.toString());
+    }
+}
+//China Standard Time(CsT)
