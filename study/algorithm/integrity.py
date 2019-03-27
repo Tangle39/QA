909fa5aca3a83d3361008c4169ac3f6d7c08efde
なな <137413469@qq.com>
2019-03-27 21:04:52 +0800

    Create integrity.py

---
+++ study/algorithm/integrity.py
@@ -0,0 +1,11 @@
+
+# -*- coding:utf-8 -*-
+# print bin(13)
+# print 5>>1
+#快速幂算法太烦
+class Solution:
+    def Power(self, base, exponent):
+        # write code here
+        return base**exponent
+s =Solution()
+print s.Power(2,-3)
