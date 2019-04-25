e8e7bbe867f5290fac0fe13b7737891525664cdf
なな <137413469@qq.com>
2019-04-25 21:42:49 +0800

    Update test_env.py

--- study/py/3/test_env.py
+++ study/py/3/test_env.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-#指定解释器
+#指定解释器(unix system)
 ##!/usr/bin/env/ python3 表示从 "PATH 环境变量"中查找 python3 解释器的位置,
 # 路径没有被写死, 而是在"环境变量"中寻找 python3 解释器的安装路径, 再调用该路径下的解释器来执行脚本.
 print(3/2)
