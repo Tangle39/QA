9881f37a5b29e97f78d2bc3e2f7b469fd785139b
あの子 <137413469@qq.com>
2019-06-27 14:45:35 +0800

    Create position_create.py

---
+++ back-end/position_create.py
@@ -0,0 +1,26 @@
+'''
+批量增加岗位
+岗位名即自增的整数
+'''
+import requests
+
+
+def create_position():
+    data = {
+        "request": {
+            "c": "",
+            "m": "",
+            "p": {
+                "session": "eyJmcm9tIjoiQiIsInNyY19pZCI6MSwibWFuYWdlcl9pZCI6ODAsImNvbXBhbnlfaW****gwLCJ1c2VyX2lkIjo3NCwiZXhwaXJlIjoxNTYyMTU2NTA0LCJzaWduYXR1cmUiOiJiM2U5ZWZhMmIzMzIwN2M3ZWI1ZjRjMGEyMTdiYmMxZjhlMzIzNDdiIiwidXNlcl90eXBlIjoxfQ==",
+                "name": i
+            }
+        }
+    }
+
+    url = 'https://bot.****.ifchange.com/api/dhr_manager/positions/create'
+    r = requests.post(url, json=data, verify=False)
+    print(r.text)
+
+
+for i in range(1, 1001):
+    create_position()
