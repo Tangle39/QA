e7cd40a612477f641bb94f488bbd54b51abe8786
Tangle39 <137413469@qq.com>
2023-02-19 12:59:48 +0800

    Update tool.py

--- learn/source/tool.py
+++ learn/source/tool.py
@@ -11,6 +11,27 @@ def trans(string: str):
     return res
 
 
+def fibonacci(n):
+    """
+    基本的dp和生成器
+    """
+    if not isinstance(n, int):
+        raise TypeError("n must be an integer")
+    if n <= 0:
+        raise ValueError("n must be positive")
+
+    def fib():
+        a, b = 0, 1
+        while True:
+            yield a
+            a, b = b, a + b
+
+    for idx, res in enumerate(fib()):
+        if idx == n-1:
+            return res
+
+
 if __name__ == '__main__':
     source = 'Multi-Domain Subsystem'
     print(trans(source))
+    print(fibonacci(10))
